58d394f14eb620e526ed517367bb20e1d62eae79
KariukiSAN <kariukian1@gmail.com>
2023-10-09 15:54:05 +0300

    A1

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    # Use list comprehension to filter even numbers from num_list
+    evens = [num for num in num_list if num % 2 == 0]
+    return evens
 
 def make_exclamation(sentence_list):
-    pass
-
+    # Use list comprehension to add exclamation marks to sentences
+    exclamation_sentences = [sentence + '!' for sentence in sentence_list]
+    return exclamation_sentences
